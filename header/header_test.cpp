6d5883fd709c4bb013248ee1346dbfce6c946f92
wuzhanqiao <wuzhanqiao@dm-ai.cn>
2021-08-15 06:50:15 +0800

    The example of header file

---
+++ header/header_test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "math.h"
+using namespace std;
+int main()
+{
+	int a = 4; int b = 5;
+	int res = add(a, b);
+	cout << a << " + " << b << " = " << res << endl;
+	return 0;
+}
